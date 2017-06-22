6e14c41846bf40cb7e504f6bbce31cde71bc5995
rohinsha <r2hinsha@gmail.com>
2017-06-22 09:16:29 +0530

    Responsive design adjustments

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,46 @@ $(document).ready(function() {
 
   /* wow
   -------------------------------*/
-  new WOW({ mobile: false }).init();
+  new WOW({ mobile: true }).init();
 
   });
 
+// wait until DOM is ready
+/*document.addEventListener("DOMContentLoaded", function(event) {
+
+  function ground() {
+
+    var tl = new TimelineMax({
+      repeat: -1
+    });
+
+    tl.to(".about-thumb", 20, {
+        backgroundPosition: "1301px 0px",
+        force3D:true,
+        rotation:0.01,
+        z:0.01,
+        autoRound:false,
+        ease: Linear.easeNone
+      });
+
+    return tl;
+  }
+
+  
+  var masterTL = new TimelineMax({
+    repeat: -1
+  });
+  
+  // window load event makes sure image is 
+// loaded before running animation
+window.onload = function() {
+  
+  masterTL
+  .add(ground(),0)
+  .timeScale(0.7)
+  .progress(1).progress(0)
+  .play();
+
+};
+  
+});*/
